97c2891ebae1994e02b657193ba4fb0bf019bced
LeeTaeWoong <bugkingus@gmail.com>
2019-11-25 21:23:49 +0900

    committed

--- src/index.js
+++ src/index.js
@@ -1,10 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "Components/App";
+import { injectGlobal } from "styled-components";
+import reset from "styled-reset";
+import "./typography";
 
+injectGlobal`
+${reset};
+body{
+    background-color:#ecf0f1;
+}`;
 
-ReactDOM.render(<App />, document.getElementById('root'));
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
+ReactDOM.render(<App />, document.getElementById("root"));
